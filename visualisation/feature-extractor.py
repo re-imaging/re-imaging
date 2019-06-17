3e6cd84a4d4434cd2bdeeae2ce51adc5077a2676
kynantan <kynan.tan@unsw.edu.au>
2019-06-17 18:28:40 +1000

    added python scripts for features and tSNE

---
+++ visualisation/feature-extractor.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[74]:
+
+
+import os
+import random
+
+
+# In[75]:
+
+
+from PIL import Image
+# from pillow import Image
+
+
+# In[76]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# In[77]:
+
+
+import pickle
+
+
+# In[78]:
+
+
+import time
+
+
+# In[79]:
+
+
+import keras
+from keras.preprocessing import image
+from keras.applications.imagenet_utils import decode_predictions, preprocess_input
+from keras.models import Model
+
+
+# In[80]:
+
+
+model = keras.applications.VGG16(weights='imagenet', include_top=True)
+
+
+# In[81]:
+
+
+print("model loaded")
+model.summary()
+
+
+# In[82]:
+
+
+def load_image(path):
+    img = image.load_img(path, target_size=model.input_shape[1:3])
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    return img, x
+
+
+# In[83]:
+
+
+# set up the feature extractor
+
+feat_extractor = Model(inputs=model.input, outputs=model.get_layer("fc2").output)
+print("feature extractor setup")
+feat_extractor.summary()
+
+
+# In[84]:
+
+
+# create a list of all the category/year folders
+
+paths = []
+
+parent_dir = '/home/rte/data/images/cat/'
+category = ''
+year = ''
+
+categories = [f.path for f in os.scandir(parent_dir) if f.is_dir()]
+# print(categories)
+
+for c in categories:
+    path = [f.path for f in os.scandir(c) if f.is_dir()]
+#     print(path)
+    paths.append(path)
+
+# print(paths)
+for p in paths:
+    p.sort()
+print("----- paths -----")
+print(paths)
+
+
+# In[ ]:
+
+
+# big loop here
+
+for cat in paths:
+    for d in cat:
+        current_path = d
+        print("current path: " + current_path)
+        category = current_path.split('/')[6]
+        year = current_path.split('/')[7]
+        print("category: " + category)
+        print("year: " + str(year))
+        
+        image_extensions = ['.jpg', '.png', '.jpeg']   # case-insensitive (upper/lower doesn't matter)
+
+        images = [os.path.join(dp, f) for dp, dn, filenames in os.walk(current_path) for f in filenames if os.path.splitext(f)[1].lower() in image_extensions]
+        num_x = len(images)
+        print("keeping %d images to analyze" % num_x)
+        
+        
+        
+        tic = time.clock()
+
+        features = []
+        for i, image_path in enumerate(images):
+            if i % 500 == 0:
+                toc = time.clock()
+                elap = toc-tic;
+                print("analyzing image %d / %d. Time: %4.4f seconds." % (i, len(images),elap))
+                tic = time.clock()
+            img, x = load_image(image_path)
+            
+            
+            feat = feat_extractor.predict(x)[0]
+            features.append(feat)
+
+        print('finished extracting features for %d images' % len(images))
+
+        # write images, features to a pickle file
+
+        f = "features_" + category + "_" + year + "_vgg_x" + str(num_x) + ".pickle"
+
+        print(f)
+
+        # WRITE
+        with open(f, "wb") as write_file:
+            pickle.dump([images, features], write_file)
+            write_file.close()
+
+
+# In[ ]:
+
+
+
+
---
+++ visualisation/tSNE-cat-year.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[38]:
+
+
+import os
+import random
+import pickle
+import time
+import datetime
+
+
+# In[26]:
+
+
+from PIL import Image
+# from pillow import Image
+
+
+# In[27]:
+
+
+from matplotlib.pyplot import imshow
+
+
+# In[28]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# In[29]:
+
+
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+
+
+# In[30]:
+
+
+from matplotlib.pyplot import imshow
+
+
+# In[31]:
+
+
+perp = 50
+bPCA = True
+num_iterations = 2000
+
+
+# In[33]:
+
+
+pickle_folder = '/home/rte/re-imaging/visualisation/'
+
+paths = []
+
+for file in os.listdir(pickle_folder):
+    if file.endswith(".pickle"):
+        paths.append(os.path.join(pickle_folder, file))
+paths.sort()
+# print(paths)
+
+for p in paths:
+    print(p)
+
+
+# In[ ]:
+
+
+# loop here
+
+for p in paths:
+    print(p)
+    
+    category = p.split('_')[1]
+    year = p.split('_')[2]
+    print("category: " + category)
+    print("year: " + str(year))
+
+    with open(p, "rb") as read_file:
+        images, features = pickle.load(read_file)
+        read_file.close()
+    
+    # check that we still have the features and list of images
+    print("----- checking images and features -----")
+    print("length of images: " + str(len(images)))
+    print("length of features: " + str(len(features)))
+    for img, f in list(zip(images, features))[0:5]:
+        print("image: %s, features: %0.2f,%0.2f,%0.2f,%0.2f... "%(img, f[0], f[1], f[2], f[3]))
+    
+    if len(images) >= 300:
+        features = np.array(features)
+        print("----- running pca across features -----")
+        pca = PCA(n_components=300)
+        pca.fit(features)
+
+        pca_features = pca.transform(features)
+
+        X = np.array(pca_features)
+        X.shape
+        tsne = TSNE(n_components=2, learning_rate=150, perplexity=perp, angle=0.2, verbose=2, n_iter=num_iterations).fit_transform(X)
+
+        # normalise points
+        tx, ty = tsne[:,0], tsne[:,1]
+        tx = (tx-np.min(tx)) / (np.max(tx) - np.min(tx))
+        ty = (ty-np.min(ty)) / (np.max(ty) - np.min(ty))
+
+        width = 4000
+        height = 3000
+        max_dim = 100
+
+        full_image = Image.new('RGBA', (width, height))
+        for img, x, y in zip(images, tx, ty):
+            tile = Image.open(img)
+            rs = max(1, tile.width/max_dim, tile.height/max_dim)
+            tile = tile.resize((int(tile.width/rs), int(tile.height/rs)), Image.ANTIALIAS)
+            full_image.paste(tile, (int((width-max_dim)*x), int((height-max_dim)*y)), mask=tile.convert('RGBA'))
+
+        plt.figure(figsize = (16,12))
+        imshow(full_image)
+
+        ts = time.time()
+        st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H-%M-%S')
+        filename = "tSNE_" + category + "_" + year + "_n" + str(num_iterations) + "_p" + str(perp) + "_" + st
+        print(filename)
+        full_image.save(filename + ".png")
+
+
+# In[ ]:
+
+
+# !pip install -U git+https://github.com/bmcfee/RasterFairy/ --user
+# import rasterfairy
+
