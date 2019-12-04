85ee1625dc7ba8ed820ed5a6de7474e2cff297d3
kynantan <kynan.tan@unsw.edu.au>
2019-12-04 17:35:39 +1100

    checking oai scripts functioning correctly

--- oai-metadata-scripts/find_record.py
+++ oai-metadata-scripts/find_record.py
@@ -1,38 +1,39 @@
 import xml.etree.ElementTree as ET
 import glob
 import datetime
+import argparse
+
+parser = argparse.ArgumentParser(description='Utility script for finding a specific article record in OAI metadata')
+
+# also useful for parsing over large numbers of items and checking for data integrity
+
+parser.add_argument('oai_path', help='set folder of OAI xml files')
+parser.add_argument("--input_id", type=str, help="arXiv identifier to search for")
+parser.add_argument('-v', '--verbose', action='store_true', help='verbose output')
+
+global args
+args = parser.parse_args()
+
+oai_path = args.oai_path
 
 count = 0
 deleted = 0
 
-# bVerbose = True
-bVerbose = False
-
 OAI = "{http://www.openarchives.org/OAI/2.0/}"
 ARXIV = "{http://arxiv.org/OAI/arXiv/}"
 
-inputID = input("Enter an arXiv identifier: ")
-# inputID = "1804.04861"
-# inputID = "9912209"
-# inputID = "0804.3168"
-# inputID = "1710.10269"
+if args.input_id is None:
+    inputID = input("Enter an arXiv identifier: ")
+else:
+    inputID = args.input_id
 
 print("searching for " + inputID)
 
-# filenames = ["2018-04-30-00000011.xml"]
-filenames = ["2008-04-30-00000000.xml", "2008-04-30-00000001.xml", "2008-04-30-00000002.xml"]
-
-for filename in glob.glob('*.xml'):
-# for filename in filenames:
-
+for filename in glob.glob(oai_path + '*.xml'):
     data = ET.parse(filename)
-    if(bVerbose):
+    if(args.verbose):
         print("opening file: " + filename)
     root = data.getroot()
-
-    # for identifier in root.iter('arXiv'):
-    #     print("found an id")
-    #     print(identifier.text)
 
     for record in root.findall('.ListRecords/record'):
         try:
@@ -45,17 +46,15 @@ for filename in glob.glob('*.xml'):
 
             identifier = meta.find(ARXIV+"arXiv/").text
 
-            # print(ARXIV+"arXiv/"+"id")
-
             # this gets the arXiv element and then gets the text
             # for some reason it grabs the identifier ^_^
-            if(bVerbose):
+            if(args.verbose):
                 print(identifier)
                 print(created)
                 print(categories)
 
             if(inputID in identifier):
-                print("-" * 20)
+                print("*" * 20)
                 print("FOUND RECORD:")
                 print("-" * 20)
                 # print the important parts of the record
@@ -63,39 +62,15 @@ for filename in glob.glob('*.xml'):
                 print(created)
                 print(categories)
                 print(filename)
-                print("-" * 20)
+                # print("-" * 20)
         except KeyboardInterrupt:
             # quit
             sys.exit()
         except:
             deleted += 1
-            if(bVerbose):
+            if(args.verbose):
                 print("error! perhaps a record was deleted?")
             continue
 
 print("*" * 20)
-print("total number of deleted items: " + str(deleted))
-
-        # .find('id').text)
-
-        # print(meta)
-        # for node in meta:
-        #     print(node)
-
-        # for identifier in record.findall('./metadata/{http://arxiv.org/OAI/arXiv/}/id'):
-        # for metadata in record.findall('./metadata/'):
-        #     print("got metadata")
-        #     print(metadata)
-        #
-        #     print("getting nodes")
-        #     identifier = metadata.findall
-
-            # for node in metadata:
-                # print(node)
-    #     print(record.tag, record.attrib)
-    #     identifier = record.find('./metadata/arXiv')
-    #     print(identifier)
-    #     print(identifier.tag, identifier.attrib)
-        # for identifier in record.findall('./metadata/arXiv/id'):
-        #     print("identifier: " + identifier)
-        #     print(identifier.attrib)
+print("total number of erroneous (possible deleted) items: " + str(deleted))
