8eba903804607a58ac8974721340f24d18716969
kynantan <kynan.tan@unsw.edu.au>
2021-06-18 17:42:43 +1000

    move all imagemesh files to new repo

--- interactive/imagemesh/install.sh
+++
@@ -1,4 +0,0 @@
-python3 -m venv env
-source env/bin/activate
-python -m pip install -U pip
-pip install numpy annoy flask Flask-Markdown
--- interactive/imagemesh/run.py
+++
@@ -1,5 +0,0 @@
-from imagemesh import app
-
-if __name__ == "__main__":
-    # app = imagemesh.create_app()
-    app.run()
